bd7a69159f7ef68b927f842395c70f72c9327b06
Kauã H <kaueheberle@gmail.com>
2025-08-09 22:40:35 -0300

    Remake

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const usuarioRoutes = require('./routes/usuario.routes');
+const authRoutes = require('./routes/auth.routes');
+const seedAdmin = require('./utils/seedAdmin');
 
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(authRoutes);
+app.use('/usuarios', usuarioRoutes);
+
+
+seedAdmin();
+
+main
+app.get('/api', (req, res) => {
+  res.send('API da Academia está rodando! 🏋');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
